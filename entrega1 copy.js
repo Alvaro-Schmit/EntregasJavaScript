6e2dc5648cd8dd5ed2212547743a913d62023c0f
Alvaro Schmit <alvaroschmit@gmail.com>
2021-02-01 13:09:07 -0300

    agregue copias de index y .js

---
+++ entrega1 copy.js
@@ -0,0 +1,12 @@
+let tituloSubtitulo = document.getElementById('encabezado');
+let title = document.createElement('h1')
+title.textContent = 'Bienvenido';
+tituloSubtitulo.appendChild(title);
+
+
+let subtituloCotizar = document.createElement('h2');
+subtituloCotizar.textContent = 'Utiliza esta plantilla para cotizar tu trabajo';
+tituloSubtitulo.appendChild(subtituloCotizar);
+
+title.style.textAlign = 'center';
+subtituloCotizar.style.textAlign = 'center';
