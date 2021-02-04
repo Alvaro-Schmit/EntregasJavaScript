92df7aebb0e4ccf8a485205cd464b99cb0a7d87a
Alvaro Schmit <alvaroschmit@gmail.com>
2021-02-04 19:16:42 -0300

    no se como hacer un div en una class?

--- datos.js
+++ datos.js
@@ -2,41 +2,41 @@ const pageStructure = [{
         "pregunta": "Ingresa los metros a pintar",
         "img": "https://placeimg.com/280/80/meters",
         "input": `<input type="text" name="firtsName" id="keysearch01" />`,
-        "especificaciones": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, numquam quos architecto vitae, repudiandae ex earum molliti'
+        "especificaciones": 'consectetur adipisicing elit. Accusamus, numquam quos architecto vitae, repudiandae ex earum molliti'
     },
 
     {
         "pregunta": "Ingresa la textura de la superficie",
         "img": "https://placeimg.com/280/80/Texture",
         "input": `<input type="text" name="firtsName" id="keysearch01" />`,
-        "especificaciones": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, numquam quos architecto vitae, repudiandae ex earum molliti'
+        "especificaciones": 'consectetur adipisicing elit. Accusamus, numquam quos architecto vitae, repudiandae ex earum molliti'
     },
 
     {
         "pregunta": "Ingresa el color original de la superficie",
         "img": "https://placeimg.com/280/80/colors",
         "input": `<input type="text" name="firtsName" id="keysearch01" />`,
-        "especificaciones": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, numquam quos architecto vitae, repudiandae ex earum molliti'
+        "especificaciones": 'consectetur adipisicing elit. Accusamus, numquam quos architecto vitae, repudiandae ex earum molliti'
     },
     {
         "pregunta": "Ingresa las caracteristicas del color",
         "img": "https://placeimg.com/280/80/architecture",
         "input": `<input type="text" name="firtsName" id="keysearch01" />`,
-        "especificaciones": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, numquam quos architecto vitae, repudiandae ex earum molliti'
+        "especificaciones": 'consectetur adipisicing elit. Accusamus, numquam quos architecto vitae, repudiandae ex earum molliti'
     },
 
     {
         "pregunta": "Ingresa el color que se aplicará",
         "img": "https://placeimg.com/280/80/ingresa el color que se aplicará",
         "input": `<input type="text" name="firtsName" id="keysearch01" />`,
-        "especificaciones": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, numquam quos architecto vitae, repudiandae ex earum molliti'
+        "especificaciones": 'consectetur adipisicing elit. Accusamus, numquam quos architecto vitae, repudiandae ex earum molliti'
     },
 
     {
         "pregunta": "Ingresa sus caracteristicas",
         "img": "https://placeimg.com/280/80/peints",
         "input": `<input type="text" name="firtsName" id="keysearch01" />`,
-        "especificaciones": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, numquam quos architecto vitae, repudiandae ex earum molliti'
+        "especificaciones": 'consectetur adipisicing elit. Accusamus, numquam quos architecto vitae, repudiandae ex earum molliti'
     }
 ]
 
--- entrega1 copy.js
+++ entrega1 copy.js
@@ -1,24 +1,40 @@
-function crearTitulo() {
-    let tituloSubtitulo = document.getElementById('encabezado');
-    let title = document.createElement('h1')
-    title.textContent = 'Bienvenido';
-    tituloSubtitulo.appendChild(title);
-
-    let subtituloCotizar = document.createElement('h2');
-    subtituloCotizar.textContent = 'Utiliza esta plantilla para cotizar tu trabajo';
-    tituloSubtitulo.appendChild(subtituloCotizar);
-
-    title.style.textAlign = 'center';
-    subtituloCotizar.style.textAlign = 'center';
-
-}
-window.addEventListener('load', function() {
-    crearTitulo();
-})
-
 //crear targetas desde el DOM
 
 
+class DOMBuild {
+    constructor() {
+
+    }
+    h4(preguntaClass) {
+        const h4 = document.createElement('h4');
+        h4.textContent = preguntaClass;
+
+        return h4;
+    }
+
+
+
+    img(imgClass) {
+            const imgConst = document.createElement('img');
+            imgConst.src = imgClass;
+            return imgConst;
+        }
+        //     divparrafo(divparrafo) {
+        //         const divparrafo = document.createElement('div');
+
+    // }
+    // parr(parrafo) {
+    //     const pEspecificacion = document.createElement('p');
+    //     pEspecificacion.textContent = parrafo;
+    //     return pEspecificacion;
+
+    // }
+
+
+
+}
+
+////////
 
 function buildSection(product) {
 
@@ -26,14 +42,10 @@ function buildSection(product) {
     const flexItemBody = document.createElement('div');
     flexItemBody.classList.add("flexItemBody");
 
-    const h4questionData = document.createElement('h4');
-    h4questionData.textContent = product.pregunta;
-
+    const h4questionData = dombuilder.h4(product.pregunta);
     const inputData = document.createElement('input');
-    inputData.classList.add('input');
+    const image = dombuilder.img(product.img);
 
-    const image = document.createElement('img');
-    image.src = product.img;
 
     const divparrafo = document.createElement('div');
     const pEspecificacion = document.createElement('p');
@@ -49,10 +61,16 @@ function buildSection(product) {
     return flexItemBody;
 
 }
+const dombuilder = new DOMBuild();
+console.log(dombuilder);
+
+
 
 
 window.addEventListener('load', function() {
     const flexContainerBodyjs = document.getElementById('flexContainerBody02');
+    const dombuilder = new DOMBuild();
+    console.log(dombuilder);
 
     pageStructure.forEach(function(product) {
         const card = buildSection(product);
